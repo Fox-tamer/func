e3d0c3a06cd079a675faa70c0afb325543e8ab0c
Alauddin EL-Safty <alaaud1007@gmail.com>
2024-11-13 00:05:39 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// A function that does a mathematical operation and returns an output
+int x(int a, int b) {
+    return a *b/2;
+}
+
+int main() {
+    int result = x(4, 60);
+    cout << "Result = " << result << endl;
+
+}
